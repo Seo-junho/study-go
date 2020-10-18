60af4dfd13175c3c522684bea2fe1dc3cce13dab
jun_ho98 <jun_ho98@naver.com>
2020-10-18 15:33:53 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"awesomeProject/accounts/mydict"
+	"fmt"
+)
+
+func main() {
+	dictionary := mydict.Dictionary{"first" : "First word"}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	err := dictionary.Delete(baseWord)
+	word, err2 := dictionary.Search(baseWord)
+	if err == nil{
+		fmt.Println(err2)
+	}
+	fmt.Println(word)
+
+}
