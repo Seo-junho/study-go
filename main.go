d2600aa48a27dc1c9fdea5a0d0de561f51d150d0
jun_ho98 <jun_ho98@naver.com>
2020-10-18 23:45:48 +0900

    end project

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "awesomeProject/scrapper"
+import (
+	"awesomeProject/scrapper"
+	"github.com/labstack/echo"
+	"os"
+	"strings"
+)
+
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	exportFilename := term + " jobs.csv"
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, exportFilename)
+}
 
 func main(){
-	scrapper.Scrape("term")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
